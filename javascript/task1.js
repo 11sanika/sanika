1914a86e8364cacb89efeeea22c0d574b3216e6b
Sanika Dhage <154016128+11sanika@users.noreply.github.com>
2025-03-26 23:05:47 +0530

    Delete javascript directory

--- javascript/task1.js
+++
@@ -1,5 +0,0 @@
-function reverseNumber() {
-    let num = document.getElementById("numberInput").value;
-    let reversedNum = parseInt(num.toString().split('').reverse().join(''));
-    document.getElementById("result").innerText = "Reversed Number: " + reversedNum;
-}
